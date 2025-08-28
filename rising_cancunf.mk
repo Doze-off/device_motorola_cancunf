eed8aeee3a77c09c5d377c348077f1159dee3016
Doze-off <viniciussadxxx@gmail.com>
2025-08-28 00:14:49 -0300

    Update rising_cancunf.mk

--- rising_cancunf.mk
+++ rising_cancunf.mk
@@ -1,6 +1,5 @@
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit from device makefile.
 $(call inherit-product, device/motorola/cancunf/device.mk)
